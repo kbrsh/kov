6218cd8ea1eebd6f7cd09ca2bd208b7701011448
Gopal Shah <simplecooldude1@gmail.com>
2016-10-19 15:18:00 -0700

    function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var sentence = require("./src/sentence.js");
 var word = require("./src/word.js");
 module.exports = function(data, opts) {
-
+  if(opts.type.toLowerCase() === "sentence") {
+    return sentence(data, opts);
+  }
+  if(opts.type.toLowerCase() === "word") {
+    return word(data, opts);
+  }
 }
