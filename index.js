fb241fd84207925bc65c823cf8da05dc59061f75
Gopal Shah <simplecooldude1@gmail.com>
2016-10-19 15:18:52 -0700

    else error

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var sentence = require("./src/sentence.js");
 var word = require("./src/word.js");
 module.exports = function(data, opts) {
+
   if(opts.type.toLowerCase() === "sentence") {
     return sentence(data, opts);
-  }
-  if(opts.type.toLowerCase() === "word") {
+  } else if(opts.type.toLowerCase() === "word") {
     return word(data, opts);
+  } else {
+    return "ERR: Invalid type of Markov Chain"
   }
 }
